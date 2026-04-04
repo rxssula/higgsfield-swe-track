f2dbd2e8dbdad61e182ceec1138b368eb88b664d
Rassul <rxssul.aitkali@gmail.com>
2026-04-04 16:58:23 +0500

    [feat]: add tailwind and implement home page for rooms handling

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { cloudflare } from '@cloudflare/vite-plugin'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
-	return {
-		plugins: [cloudflare(), react()],
-	}
+    return {
+        plugins: [cloudflare(), react(), tailwindcss()],
+    }
 })
